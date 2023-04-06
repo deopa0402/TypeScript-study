3c73de9f2a2adf3e603bd83980b01c0010bb13c3
deopa0402 <deopa0402@gmail.com>
2023-04-06 12:06:24 +0900

    #2.5 (04:49) Emoji❤ hover active

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const Father = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
+const animation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    transform:rotate(360deg);
+    border-radius:100px;
+  }
+  100% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
 `;
 
+const Emoji = styled.span`
+  font-size: 100px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${animation} 1s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 200px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
 function App() {
   return (
     <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <Box>
+        <Emoji as="F">❤️</Emoji>
+      </Box>
+      <Emoji>💔</Emoji>
     </Father>
   );
 }
