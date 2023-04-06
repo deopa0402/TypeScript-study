2b84425579eef3922597f267bbe585e759520896
deopa0402 <deopa0402@gmail.com>
2023-04-06 09:12:17 +0900

    styled css #2.1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import styled from 'styled-components';
 const Father = styled.div`
   display: flex;
 `;
+
 const BoxOne = styled.div`
   background-color: teal;
   width: 100px;
   height: 100px;
 `;
+
 const BoxTwo = styled.div`
+  background-color: tomato;
   width: 100px;
   height: 100px;
 `;
+
 const Text = styled.span`
   color: white;
 `;
@@ -19,10 +23,8 @@ const Text = styled.span`
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box />
+      <Box />
     </Father>
   );
 }
