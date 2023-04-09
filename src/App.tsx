3ff83ec3f8e646df896caf69485b96da391bd8b8
deopa0402 <deopa0402@gmail.com>
2023-04-09 13:00:02 +0900

    #3.6 Themes (08:42) lightTheme darkTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setvalue] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setvalue(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="usename"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>pro</H1>
+    </Container>
   );
 }
 
