cb82466eae7b1041f7ef87aaa0fae2839518d9a8
deopa0402 <deopa0402@gmail.com>
2023-04-10 19:40:36 +0900

    #41 BroweerRouter (06:33) element={<About />} or {<Home />}

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  other?: boolean;
-}
-
-function Dummy({ text, other = true }: DummyProps) {
-  return <H1>{text}</H1>;
-}
-
+import React from 'react';
+import Router from './Router';
 function App() {
   return (
-    <Container>
-      <Dummy text="안뇽!" />
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 }
 
