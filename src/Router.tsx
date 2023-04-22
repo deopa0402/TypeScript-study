d2a18e7f6b0386e7da89f0d73a8fbdd76e350942
deopa0402 <deopa0402@gmail.com>
2023-04-22 16:26:16 +0900

    주석 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,32 +1,33 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Home from './screens/Home';
-import About from './screens/About';
-import Root from './Root';
-import NotFound from './screens/NotFound';
-import ErrorComponent from './components/ErrorComponent';
-import User from './screens/users/User';
-import Followers from './screens/users/Followers';
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./screens/Home";
+import About from "./screens/About";
+import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
+import User from "./screens/users/User";
+import Followers from "./screens/users/Followers";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     children: [
       {
-        path: '',
+        path: "",
         element: <Home />,
         errorElement: <ErrorComponent />,
       },
       {
-        path: 'about',
+        path: "about",
         element: <About />,
       },
       {
-        path: 'users/:userId',
+        path: "users/:userId",
         element: <User />,
         children: [
           {
-            path: 'Followers',
+            /** 추가적으로 라우터를 만드는것이 아닌 자식을 통해서 만들 수 있음! */
+            path: "Followers",
             element: <Followers />,
           },
         ],
