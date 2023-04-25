4b5bcfaa22bebfddeea8719b21681dcadc5cd735
deopa0402 <deopa0402@gmail.com>
2023-04-25 23:42:07 +0900

    git push 에러 설명

--- src/Router.tsx
+++ src/Router.tsx
@@ -38,3 +38,11 @@ const router = createBrowserRouter([
 ]);
 
 export default router;
+
+// git push 를 하던도중 에러발생
+// error: failed to push some refs to 'https://github.com/hahwul/a2sv.git'
+// hint: Updates were rejected because the tip of your current branch is behind
+// hint: its remote counterpart. Merge the remote changes (e.g. 'git pull')
+// hint: before pushing again.
+// hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+// git push --force 로 해결하긴 했지만 원인은 무엇인지, 어떠한 현상인지 알아봐야 될것같음
