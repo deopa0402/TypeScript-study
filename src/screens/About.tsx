bc4501fcb060df2c5a1dd8efe6fbb701c3b8c3b2
deopa0402 <deopa0402@gmail.com>
2023-04-25 23:35:10 +0900

    파일들에 대한 설명,주석 추가

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,4 +1,5 @@
 function About() {
+  // /about 경로에 있는 사이트 내부를 구연하는 파일
   return <h1>About</h1>;
 }
 export default About;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
-import { users } from '../db';
+import { Link } from "react-router-dom";
+import { users } from "../db";
 
 function Home() {
   return (
     <div>
       <ul>
+        {/* ul 컴포넌트 안에 deo, nlco 를 불러옴 */}
         {users.map((user) => (
           <li key={user.id}>
             <Link to={`/users/${user.id}`}>{user.name}</Link>
